446b4dc10dc07b6715608ae4e2af0ee0a0ce7d25
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-11-11 23:42:34 +0700

    add : stable training high perf code

---
+++ ai/license-plate-auto-blur/main.py
@@ -0,0 +1,34 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":  # Multiprocessing guard
+    
+    model = YOLO("yolo11n.pt")
+    
+    model.train(
+        data="data.yaml",
+        epochs=0,
+        imgsz=640,
+        batch=-1,  # Auto ~60% VRAM
+        device=0,
+        multi_scale=True,  # Enabled for scale robustness
+        cache=True,  # 'ram' for 32GB speed
+        single_cls=True, # True for single class detection
+        cos_lr=True,
+        workers=8,
+        project='.', # need to prevent path issues
+        name='exported' # need to make sure folder is correct
+    )
+    
+    model.val()
+
+    model.export(
+        format="engine", 
+        half=True, 
+        dynamic=True, 
+        nms=True, 
+        device=0, 
+        workspace=8
+    )
+    
+
+
